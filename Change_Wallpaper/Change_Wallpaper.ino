bfa038d06f81a60142c9901fb3c2c45645cdf42d
Carlos Ramos <r4mos@users.noreply.github.com>
2022-01-31 23:59:44 +0100

    Create a wallpaper changer

--- Change_Wallpaper/Change_Wallpaper.ino
+++ Change_Wallpaper/Change_Wallpaper.ino
@@ -1,26 +1,21 @@
-#include "DigiKeyboard.h"
+#define kbd_es_es
+#include <DigiKeyboard.h>
 
 void setup() {
   DigiKeyboard.delay(1000);
   DigiKeyboard.sendKeyStroke(0,MOD_GUI_LEFT);
   DigiKeyboard.delay(100);
-  DigiKeyboard.println("cmd");
+  DigiKeyboard.println("powershell");
   DigiKeyboard.delay(500);
   DigiKeyboard.sendKeyStroke(67,KEY_ENTER);
   DigiKeyboard.delay(500);
-  DigiKeyboard.println("mode con: cols=16 lines=1&color FE");
-  DigiKeyboard.delay(100);
-  DigiKeyboard.println("cd Desktop&powershell (wget '<your_image_url>' -outfile s0m3fi13n4m3.jpg)"); //Add image
-  DigiKeyboard.delay(8000); //Waits for image to download
-  DigiKeyboard.sendKeyStroke(7,MOD_GUI_LEFT);
-  DigiKeyboard.delay(200);
-  DigiKeyboard.print("s0m3fi13n4m3");
-  DigiKeyboard.sendKeyStroke(67,MOD_SHIFT_LEFT);
-  DigiKeyboard.delay(100);
-  DigiKeyboard.println("b");
-  DigiKeyboard.sendKeyStroke(43,MOD_ALT_LEFT);
-  DigiKeyboard.delay(100);
-  DigiKeyboard.println("del s0m3fi13n4m3.jpg&exit");
+  DigiKeyboard.println("Invoke-WebRequest -Uri 'https://github.com/r4mos/Digispark-Duckduino/raw/master/Change_Wallpaper/wallpaper.jpg' -OutFile $env:TEMP/61772051-4de4-4c99-8842-6ec7db0a9535.jpg");
+  DigiKeyboard.delay(500);
+  DigiKeyboard.println("reg add 'HKEY_CURRENT_USER\\Control Panel\\Desktop' /v Wallpaper /t REG_SZ /d $env:TEMP/61772051-4de4-4c99-8842-6ec7db0a9535.jpg /f");
+  DigiKeyboard.delay(500);
+  DigiKeyboard.println("RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters");
+  DigiKeyboard.delay(500);
+  DigiKeyboard.println("exit");
 }
 
 void loop() {
--- Startup_prank/README.md
+++
@@ -1,4 +0,0 @@
-# Startup prank for Windows (Tested on Windows 10)
-Creates a bat file that opens a url 10 times with Google Chrome
-
-Why do that? Because I can.
